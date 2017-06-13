e0af89caa7b24f1c3e900fb21001ff1e30b8b53b
kokoriru <kokorirury@yahoo.co.jp>
2017-06-14 01:26:06 +0900

    Add signup route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
